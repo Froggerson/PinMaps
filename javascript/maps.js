97760b544ac44dadbadeda7f43ae024a313afa06
Jennifer Pham <phamjennifer314@gmail.com>
2019-07-25 13:26:37 -0700

    Fixed a bug where the map markers were back to the default ones

--- javascript/maps.js
+++ javascript/maps.js
@@ -8,6 +8,22 @@ L.tileLayer(
     }).addTo(map);
 
 // Attach events.
+//Marker
+var happyPin = L.icon({
+    iconUrl: "https://image.flaticon.com/icons/svg/1559/1559161.svg",
+    shadowUrl: '',
+//16, 1
+    iconSize:     [50, 50], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [10, 40], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+// Attach events.
+
+map.on('click', function(e) {
+    L.marker(e.latlng, {draggable: true, markerId: 9999}).addTo(map);
+    L.marker(e.latlng, {draggable: true, markerId: 9999, icon: happyPin}).addTo(map);
 
 map.on('click', function(e) {
     L.marker(e.latlng, {draggable: true, markerId: 9999}).addTo(map);
