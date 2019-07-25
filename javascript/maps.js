c1c36845dee7cd02c5efdcdf68911904c3d78919
Thomas Tran <timtom9801@gmail.com>
2019-07-25 15:33:27 -0700

    Added JINJA2 onto maps.html and main.py so that the pins in the data can be accessed and put onto the map

--- javascript/maps.js
+++ javascript/maps.js
@@ -1,4 +1,4 @@
-var map = L.map('canvas').setView([30.5, 9.48], 3);
+var map = L.map('canvas').setView([38.907192, -77.036873], 3);
 mapLink =
     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
@@ -8,20 +8,9 @@ L.tileLayer(
     }).addTo(map);
 
 // Attach events.
-//Marker
-var happyPin = L.icon({
-    iconUrl: "https://image.flaticon.com/icons/svg/1559/1559161.svg",
-    shadowUrl: '',
-//16, 1
-    iconSize:     [50, 50], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [10, 40], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-});
-// Attach events.
+
 map.on('click', function(e) {
-    L.marker(e.latlng, {draggable: true, markerId: 9999, icon: happyPin}).addTo(map);
+    L.marker(e.latlng, {draggable: true, markerId: 9999}).addTo(map);
     saveData(e.latlng);
 });
 
@@ -44,20 +33,20 @@ function saveData(latlng) {
     });
 }
 
-function getAllMarkers() {
-      var allMarkers = [];
-
-    map.eachLayer(function (layer) {
-
-        if (layer.options.markerId != undefined) {
-            // The layer is a marker.
-            allMarkers.push({
-                markerId: layer.options.markerId,
-                latlng: null
-            });
-        }
-
-    });
-
-    return allMarkers;
-}
+// function getAllMarkers() {
+//       var allMarkers = [];
+//
+//     map.eachLayer(function (layer) {
+//
+//         if (layer.options.markerId != undefined) {
+//             // The layer is a marker.
+//             allMarkers.push({
+//                 markerId: layer.options.markerId,
+//                 latlng: null
+//             });
+//         }
+//
+//     });
+//
+//     return allMarkers;
+// }
--- main.py
+++ main.py
@@ -19,20 +19,24 @@ class MapPage(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('templates/maps.html')
         current_user = users.get_current_user()
-        user_pins = Pin.get_by_user(current_user)
-
-        self.response.write(template.render())
+        user_pins = Pin.query().filter(Pin.user_id == current_user.user_id()).fetch()
+        dict_for_template = {'user_pins': user_pins}
+        # for each_pin in user_pins:
+        #     pin_query_lat = each_pin["lat"]
+        #     pin_query_lng = each_pin["lng"]
+        #     L.marker([pin_query_lat, pin_query_lng], {icon:happyPin}).addTo(map);
+        self.response.write(template.render(dict_for_template))
 
 class PinHandler(webapp2.RequestHandler):
     def post(self):
         lat_lng = json.loads(self.request.body)
+        print(lat_lng)
         pin_lat = lat_lng["lat"]
         pin_long = lat_lng["lng"]
         pin_record = Pin(latitude = pin_lat, longitude = pin_long)
         pin_record.user_id = users.get_current_user().user_id()
         pin_record.put()
 
-        self.response.write("")
 
 class AboutPage(webapp2.RequestHandler):
      def get(self):
