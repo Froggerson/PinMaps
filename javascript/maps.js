56fe222699163158fbfc4d82f820d4ad03baeea2
Jennifer Pham <phamjennifer314@gmail.com>
2019-07-25 10:15:41 -0700

    Made the pins happier :)

--- javascript/maps.js
+++ javascript/maps.js
@@ -1,3 +1,4 @@
+
 var map = L.map('canvas').setView([38.907192, -77.036873], 3);
 mapLink =
     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
@@ -7,10 +8,21 @@ L.tileLayer(
     maxZoom: 18,
     }).addTo(map);
 
+//Marker
+var happyPin = L.icon({
+    iconUrl: "https://image.flaticon.com/icons/svg/1014/1014719.svg",
+    shadowUrl: '',
+//16, 1
+    iconSize:     [50, 50], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [34, 49], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 // Attach events.
 
 map.on('click', function(e) {
-    L.marker(e.latlng, {draggable: true, markerId: 9999}).addTo(map);
+    L.marker(e.latlng, {draggable: true, markerId: 9999, icon: happyPin}).addTo(map);
 });
 
 map.on('mouseup', function(e) {
